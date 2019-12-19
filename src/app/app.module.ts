d4b3663c0b9bced6dc92640f014b4936af1fb553
baranchugov.alexandr <baranchugov.alexandr@otr.ru>
2019-12-19 17:40:06 +0300

    Bugfix: last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {GithubSearchModule} from './github-search/github-search.module';
-import { NgZorroAntdModule, NZ_I18N, ru_RU } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import ru from '@angular/common/locales/ru';
-
-registerLocaleData(ru);
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,10 +11,8 @@ registerLocaleData(ru);
   imports: [
     BrowserModule,
     GithubSearchModule,
-
     BrowserAnimationsModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: ru_RU }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
